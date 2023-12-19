1386c8a2624759d3e36850d91ea7cf4f7c9f10f3
rahul_yadav7799 <74658609+rahul-2004-json@users.noreply.github.com>
2023-12-20 02:06:43 +0530

    Inject CSS/JS to website div & elements

--- style.js
+++ style.js
@@ -1,7 +1,32 @@
-function bgclr()
-{
-    document.body.style.backgroundColor= "#04364A";
-    document.body.style.backgroundImage= "image.jpg";
-}
+document.querySelector("#header > div:nth-child(1) > a > img").classList.add("logo-cp");
+document.querySelector("#body > div.roundbox.menu-box.borderTopRound.borderBottomRound").classList.add("Navigation-bar");
+document.querySelector("#header > div.lang-chooser > div:nth-child(2) > a:nth-child(1)").classList.add("username-logout");
+document.querySelector("#header > div.lang-chooser > div:nth-child(2) > a:nth-child(2)").classList.add("username-logout");
+document.querySelector("#header > div.lang-chooser > div:nth-child(1) > div > div.header-bell__img").classList.add("icons-top");
+document.querySelector("#header > div.lang-chooser > div:nth-child(1) > a:nth-child(3) > img").classList.add("icons-top");
+document.querySelector("#header > div.lang-chooser > div:nth-child(1) > a:nth-child(4) > img").classList.add("icons-top");
+document.querySelector("body").classList.add("body-cp");
 
-bgclr() ;
+document.querySelector("#pageContent").classList.add("page-content-style");
+document.querySelector("#sidebar > div:nth-child(1)").classList.add("sub-topics");
+document.querySelector("#sidebar > div:nth-child(5)").classList.add("sub-topics");
+document.querySelector("#sidebar > div:nth-child(6)").classList.add("sub-topics");
+document.querySelector("#sidebar > div:nth-child(7)").classList.add("sub-topics");
+document.querySelector("#sidebar > div:nth-child(9)").classList.add("sub-topics");
+document.querySelector("#sidebar > div:nth-child(11)").classList.add("sub-topics");
+
+
+
+
+var elements = pageContent.querySelectorAll("#pageContent > div");
+elements.forEach(function (element) {
+    element.classList.add("page-content-div-style");
+});
+
+
+document.querySelector("#body > div:nth-child(11)");
+document.querySelector("#footer").classList.add("Footer");
+document.querySelector("#footer > div:nth-child(4)").classList.add("Footer");
+document.querySelector("#footer > div.smaller").classList.add("Footer");
+document.querySelector("#footer > div:nth-child(6)").classList.add("Footer");
+document.querySelector("#footer > div:nth-child(7)").classList.add("Footer");
